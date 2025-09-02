69c1d1bae4093247402731d44f060f1c7d88f72b
Italo Artur <italo.artur@arpe.local>
2025-09-02 09:22:20 -0300

    Refatorando Título

--- src/sections/tittle/tittle.py
+++ src/sections/tittle/tittle.py
@@ -1,27 +1,92 @@
 import os
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
 from docx.shared import Inches
-from docx.shared import Pt # Import Pt for point units
 from utils import *
 
 def gerar_titulo(doc, BASE_DIR):
-    adicionar_imagem_no_cabecalho(doc, os.path.join(BASE_DIR, "assets/logo_arpe.png"), largura=Inches(2), alinhamento=WD_PARAGRAPH_ALIGNMENT.CENTER)
-
-    adicionar_paragrafo(doc, "Relatório de Fiscalização", tamanho_fonte=16, alinhamento=WD_PARAGRAPH_ALIGNMENT.CENTER, estilo='Title' ,espaco_antes=12, espaco_depois=6, cor=(0,0,0))
-    adicionar_paragrafo(doc, "Agência Reguladora de Pernambuco - ARPE", tamanho_fonte=12, alinhamento=WD_PARAGRAPH_ALIGNMENT.CENTER, estilo='Subtitle', espaco_antes=6, espaco_depois=12, cor=(0,0,0))
+    """
+    Gera a página de título do relatório.
+    """
     
+    # Adiciona o cabeçalho e os títulos principais
+    adicionar_imagem_no_cabecalho(
+        doc,
+        os.path.join(BASE_DIR, "assets/logo_arpe.png"),
+        largura=Inches(2),
+        alinhamento=WD_PARAGRAPH_ALIGNMENT.CENTER
+    )
+    
+    adicionar_paragrafo(
+        doc,
+        "Relatório de Fiscalização",
+        tamanho_fonte=16,
+        alinhamento=WD_PARAGRAPH_ALIGNMENT.CENTER,
+        estilo='Title',
+        espaco_antes=12,
+        espaco_depois=6,
+        cor=(0, 0, 0)
+    )
+    
+    adicionar_paragrafo(
+        doc,
+        "Agência Reguladora de Pernambuco - ARPE",
+        tamanho_fonte=12,
+        alinhamento=WD_PARAGRAPH_ALIGNMENT.CENTER,
+        estilo='Subtitle',
+        espaco_antes=6,
+        espaco_depois=12,
+        cor=(0, 0, 0)
+    )
+    
+    # Adiciona a imagem principal e centraliza
     doc.add_picture(os.path.join(BASE_DIR, "assets/gas.png"), width=Inches(6))
-    logo_gas = doc.paragraphs[-1]
-    logo_gas.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+    doc.paragraphs[-1].alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
     
+    # Adiciona o título do relatório
+    adicionar_paragrafo(
+        doc,
+        "FISCALIZAÇÃO DAS INSTALAÇÕES DE GÁS NOS MUNICÍPIOS DE CABO DE SANTO AGOSTINHO E JABOATÃO DOS GUARARAPES",
+        bold=True,
+        tamanho_fonte=10,
+        estilo='Heading 1',
+        alinhamento=WD_PARAGRAPH_ALIGNMENT.CENTER,
+        espaco_antes=12,
+        cor=(0, 0, 0)
+    )
     
-    adicionar_paragrafo(doc, "FISCALIZAÇÃO DAS INSTALAÇÕES DE GÁS NOS MUNICÍPIOS DE CABO DE SANTO AGOSTINHO E JABOATÃO DOS GUARARAPES", bold=True, tamanho_fonte=10, estilo='Heading 1', alinhamento=WD_PARAGRAPH_ALIGNMENT.CENTER, espaco_antes=12, cor=(0,0,0))
-    adicionar_paragrafo(doc, "Argemiro Rivas", 10, alinhamento=WD_ALIGN_PARAGRAPH.CENTER, estilo='Heading 2', cor=(0,0,0))
-    adicionar_paragrafo(doc, "Marta Rejane", 10, alinhamento=WD_ALIGN_PARAGRAPH.CENTER, estilo='Heading 2', cor=(0,0,0))
-    adicionar_paragrafo(doc, "João Paulo Costa", 10, alinhamento=WD_ALIGN_PARAGRAPH.CENTER, estilo='Heading 2', cor=(0,0,0))
-    adicionar_paragrafo(doc, "Alexandre Almeida", 10, alinhamento=WD_ALIGN_PARAGRAPH.CENTER, estilo='Heading 2', cor=(0,0,0))
-    adicionar_paragrafo(doc, "ABRIL/2025", 10, alinhamento=WD_ALIGN_PARAGRAPH.CENTER, estilo='Heading 2', espaco_depois=20, cor=(0,0,0))
+    # Agrupa e adiciona os nomes dos autores e a data
+    autores_e_data = [
+        "Argemiro Rivas",
+        "Marta Rejane",
+        "João Paulo Costa",
+        "Alexandre Almeida",
+        "ABRIL/2025"
+    ]
+    
+    for texto in autores_e_data:
+        adicionar_paragrafo(
+            doc,
+            texto,
+            tamanho_fonte=10,
+            alinhamento=WD_PARAGRAPH_ALIGNMENT.CENTER,
+            estilo='Heading 2',
+            cor=(0, 0, 0)
+        )
+    
+    # Adiciona as informações do processo administrativo
+    informacoes_processo = [
+        "RELATÓRIO DE FISCALIZAÇÃO DIRETA PROCESSO ADMINISTRATIVO",
+        "PA-007/2025-CEEGC- GAS PROCESSOS",
+        "SEI N° 0030200024.001385/2025-99"
+    ]
 
-    adicionar_paragrafo(doc, "RELATÓRIO DE FISCALIZAÇÃO DIRETA PROCESSO ADMINISTRATIVO", 10, alinhamento=WD_ALIGN_PARAGRAPH.CENTER, estilo='Heading 1', bold=True, espaco_antes=20, cor=(0,0,0))
-    adicionar_paragrafo(doc, "PA-007/2025-CEEGC- GAS PROCESSOS", 10, alinhamento=WD_ALIGN_PARAGRAPH.CENTER, estilo='Heading 1', bold=True, cor=(0,0,0))
-    adicionar_paragrafo(doc, "SEI N° 0030200024.001385/2025-99", 10, alinhamento=WD_ALIGN_PARAGRAPH.CENTER, estilo='Heading 1', bold=True, cor=(0,0,0))
+    for texto in informacoes_processo:
+        adicionar_paragrafo(
+            doc,
+            texto,
+            tamanho_fonte=10,
+            alinhamento=WD_PARAGRAPH_ALIGNMENT.CENTER,
+            estilo='Heading 1',
+            bold=True,
+            cor=(0, 0, 0)
+        )
